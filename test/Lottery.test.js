12ae0590d488425e0f1449680e01092af79db558
Harsh <harshtanishq2002@gmail.com>
2024-07-04 15:43:19 +0530

    feat: test for reward distribution

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -81,4 +81,23 @@ describe("Lottery contarct", () => {
       assert(err);
     }
   });
+
+  it("sends money to winner and resets the array", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("1", "ether"),
+    });
+
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+    const difference = finalBalance - initialBalance;
+    // console.log(difference);
+    assert(difference > web3.utils.toWei("0.8", "ether"));
+  });
 });
